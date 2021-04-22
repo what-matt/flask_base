c4801767d92f409efdbf2f4a65e579bde4a1fba6
Matt <matt.riquelme.a@gmail.com>
2021-04-22 16:26:14 -0400

    improve: redis test

---
+++ test/test_hello.py
@@ -0,0 +1,30 @@
+import os
+
+import pytest
+import redis
+
+USERNAME = os.environ.get('APP_REDIS_USERNAME')
+PASSWORD = os.environ.get('APP_REDIS_PASSWORD')
+
+
+@pytest.fixture
+def redis_connection(app):
+    client_kwargs = {
+        "host": app.config['REDIS_HOST'],
+        "port": app.config['REDIS_PORT'],
+        "decode_responses": True
+    }
+
+    if USERNAME:
+        client_kwargs["username"] = USERNAME
+    if PASSWORD:
+        client_kwargs["password"] = PASSWORD
+
+    yield redis.Redis(**client_kwargs)
+
+
+def test_say_hello(redis_connection):
+    result = redis_connection.set("hello", "world")
+    value = redis_connection.get("hello")
+    assert result is True
+    assert value == "world"
