ebd1a42f276b62ee7df6e12189408880c132c335
Matt <matt.riquelme.a@gmail.com>
2021-04-27 20:54:08 -0400

    improve: post with flaskparser and marshmallow

--- app/api/menu.py
+++ app/api/menu.py
@@ -1,10 +1,19 @@
+from typing import Tuple
+
 from flask_restful import abort
+from webargs.flaskparser import use_args, parser
 
 from app.api.base import DaoResource
+from app.models import Menu
 from app.schema import MenuSchema
 
 class MenuListResource(DaoResource):
-  def get(self):
+    @use_args(MenuSchema, location='json')
+    def post(self, menu: Menu) -> Tuple[str, int]:
+        self.dao.insert(menu)
+        return "Accepted", 202
+
+    def get(self):
         return MenuSchema(many=True).dump(self.dao.find_all())
 
 class MenuResource(DaoResource):
@@ -13,3 +22,11 @@ class MenuResource(DaoResource):
         if not menu:
             return abort(404, message=f"Menu {menu_id} does not exist")
         return MenuSchema().dump(menu)
+
+
+@parser.error_handler
+def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
+    """webargs error handler that uses Flask-RESTful's abort function to return
+    a JSON error response to the client.
+    """
+    abort(error_status_code, errors=err.messages)
